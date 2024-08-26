aa9e5635d02797dbf2a0a0aa48aebeaf74fbbba8
Kartika Salsabila <101912097+fzlfade@users.noreply.github.com>
2024-08-26 16:44:46 +0700

    add week 2

---
+++ Week 2/fzlProject/src/main/java/com/mycompany/fzlproject/FzlProject.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fzlproject;
+
+/**
+ *
+ * @author ASUS
+ */
+public class FzlProject {
+    
+    public static void main(String[] args) {
+        String nama,NPM,alamat,no_hp;
+        
+        nama="Faizal Deshta Nugraha";
+        NPM="2340506065";
+        alamat="Seattle";
+        no_hp="08123456789";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM" + NPM);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No HP : " + no_hp);
+    }
+}
